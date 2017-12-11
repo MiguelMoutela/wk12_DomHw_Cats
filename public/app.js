320a27d52a9c3508e53e18e069abafbddd5d6481
Miguel <miguelmoutela@hotmail.com>
2017-12-11 15:14:41 +0000

    added to app

---
+++ public/app.js
@@ -0,0 +1,9 @@
+var app = function () {
+
+  const makeUnorderList = document.createElement('ul');
+    article.classList.add('cat')
+
+  const makeListItem = document.createElement('li');
+}
+
+document.addEventListener('DOMContentLoaded', app);
